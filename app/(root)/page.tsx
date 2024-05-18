f2fc62d7944aedbb1fda9ff2b8b6bcc9c6506ba3
prince-pastakiya <princepastakiya6444@.com>
2024-05-18 18:23:02 -0500

    Implement the home page with layout.
    ```

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,37 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 import React from 'react'
 
 const Home = () => {
+
+    const loggedIn = { firstName: 'Prince', lastName: 'Pastakiya', email: 'princepastakiya@gmail.com' };
+
     return (
-        <div>
-            Home
-        </div>
+        <section className='home'>
+            <div className='home-content'>
+                <header className='home-header'>
+                    <HeaderBox
+                        type="greeting" // Fix: Change "greetings" to "greeting"
+                        title="Welcome,"
+                        user={loggedIn?.firstName || 'Guest'}
+                        subtext="Access and manage your Bank Accounts and Finance."
+                    />
+                    <TotalBalanceBox
+                        accounts={[]}
+                        totalBanks={1}
+                        totalCurrentBalance={320439.78}
+                    />
+                </header>
+                RECENT TRANSACTION
+            </div>
+
+            <RightSidebar
+                user={loggedIn}
+                transactions={[]}
+                banks={[{ currentBalance: 35627.65 }, { currentBalance: 3445.89 }]}
+            />
+        </section>
     )
 }
 
